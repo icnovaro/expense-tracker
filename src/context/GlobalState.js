2acf7494e05ab3b85c21573583d37de7493c3e0d
Iván Casanova <icnovaro@gmail.com>
2020-09-14 21:11:47 -0500

    feat: creates functionality for delete and insert transactions

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,12 +4,7 @@ import AppReducer from './AppReducer';
 //InitialState
 
 const initialState = {
-    transactions: [
-        { id: 1, text: 'Flower', ammount: -20},
-        { id: 2, text: 'Salary', ammount: 300},
-        { id: 3, text: 'Book', ammount: -10},
-        { id: 4, text: 'Camera', ammount: 150}
-    ]
+    transactions: []
 };
 
 // Create Context
@@ -19,8 +14,29 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) =>  {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    return(<GlobalContext.Provider value={{transactions: state.transactions}}>
+    //Actions
+
+    function deleteTransaction(id){
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
+    function addTransaction(transaction){
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        });
+    }
+
+
+    return(<GlobalContext.Provider value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+        }}>
        {children}           
     </GlobalContext.Provider>);
-};
+};      
 
